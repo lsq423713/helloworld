1e164b3e6c4ccdde1af25a9a27f06a7409f8c233
shinoa <shinoa@DESKTOP-1QP6GDD>
2019-05-07 16:22:57 +0800

    update

--- spring-cloud-eureka/src/main/java/cn/spring/cloud/eureka/TestHutool.java
+++ spring-cloud-eureka/src/main/java/cn/spring/cloud/eureka/TestHutool.java
@@ -5,7 +5,7 @@ import cn.hutool.core.date.DateUtil;
 
 public class TestHutool {
 	public static void main(String[] args) {
-        String dateStr = "2012-12-12 12:12:12";
+        String dateStr = "2012-12-12 12:12:12随便改一下";
         Date date = DateUtil.parse(dateStr);
         System.out.println(date);
     }
